7d2f44e5b47fe25c8cc466cc9e5f7fe4f53f5507
candynaveda08 <candynaveda08@hotmail.com>
2026-05-03 11:33:52 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+let leads = [];
+
+app.post("/api/leads", (req, res) => {
+  const newLead = req.body;
+  leads.push(newLead);
+
+  console.log("Nuevo cliente:", newLead);
+
+  res.json({ message: "Guardado", lead: newLead });
+});
+
+app.get("/api/leads", (req, res) => {
+  res.json(leads);
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
